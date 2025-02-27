e60614e1e952589178ec859b00b22b4a95027240
Fermin <ferminromero2005@gmail.com>
2025-02-27 11:41:11 +0100

    Agregar script de user_data para clonar repositorio y configurar Docker para Mywall

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -4,3 +4,21 @@ sudo yum update -y
 
 # Clonar para conseguir el dockerfile
 sudo dnf install -y git
+
+cd /var
+mkdir mywall
+sudo git clone https://github.com/ferminromero00/EKS-SYMFONY.git
+
+# Mover archivos necesarios
+sudo mv EKS-SYMFONY/dockerfiles/mywall_docker mywall
+
+# Instalar docker
+sudo dnf install docker -y
+sudo systemctl start docker
+
+# Contruimos imagen
+cd /var/mywall
+sudo docker build -t mywall_symfony .
+
+# Ejecutamos la imagen
+sudo docker run -d -p 80:80 mywall-symfony
