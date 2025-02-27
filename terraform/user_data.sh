c54682fea8623346a62e8e706b6a2d1e5cca1fb5
Fermin <ferminromero2005@gmail.com>
2025-02-27 20:53:42 +0100

    Actualizar configuración de conexión a la base de datos en .env y doctrine.yaml; ajustar script user_data.sh para construir la imagen de Docker correctamente.

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -19,7 +19,7 @@ sudo systemctl enable docker
 
 # Construir la imagen
 cd /var/EKS-SYMFONY/MyWall
-# sudo docker build -t mywall_symfony -f Dockerfile_Mywall .
+sudo docker build -t mywall_symfony -f Dockerfile_Mywall .
 
 # Ejecutar el contenedor con variables de entorno
 sudo docker run -d -p 80:80 -e APP_ENV=prod -e APP_DEBUG=0 --name mywall_container mywall_symfony
