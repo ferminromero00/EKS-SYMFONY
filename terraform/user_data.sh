7b3d2b0cecff8cecc1d0c322f4a9888a3c9592d9
Fermin <ferminromero2005@gmail.com>
2025-02-27 12:58:01 +0100

    Actualizar Dockerfile_Mywall y script user_data.sh para asignar permisos adecuados y corregir la ruta de copia de archivos

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,14 +11,8 @@ sudo git clone https://github.com/ferminromero00/EKS-SYMFONY.git
 
 # Crear directorio y copiar archivos
 sudo mkdir -p /var/mywall
-sudo cp -r EKS-SYMFONY/Mywall/* /var/mywall/
+sudo cp -r EKS-SYMFONY/MyWall/* /var/mywall/
 sudo cp EKS-SYMFONY/dockerfiles/Dockerfile_Mywall /var/mywall/
-
-# Verificar archivos necesarios
-if [ ! -f /var/mywall/composer.json ]; then
-    echo "Error: composer.json no encontrado"
-    exit 1
-fi
 
 # Instalar Docker
 sudo dnf install -y docker
@@ -31,3 +25,4 @@ cd /var/mywall
 
 # Ejecutar el contenedor en segundo plano
 sudo docker run -d -p 80:80 --name mywall_container mywall_symfony
+
