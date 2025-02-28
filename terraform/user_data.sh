308a1e7572e1b586abd5afae0fd4f26842faba8b
Fermin <ferminromero2005@gmail.com>
2025-02-28 14:07:33 +0100

    Actualizar script user_data.sh para eliminar la clonación del repositorio y ejecutar el contenedor de Mywall directamente desde DockerHub.

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -5,18 +5,13 @@ sudo yum update -y
 # Instalar Git
 sudo dnf install -y git
 
-# Clonar el repositorio
-#cd /var
-# sudo git clone https://github.com/ferminromero00/EKS-SYMFONY.git
-
-# Crear directorio y copiar archivos
-# sudo mv EKS-SYMFONY/dockerfiles/Dockerfile_Mywall EKS-SYMFONY/MyWall
-
 # Instalar Docker
 sudo dnf install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Descargar el Mywall de Gerardo
+# Descargar la imagen de Mywall de DockerHub
 sudo docker pull ferminromero00/mywall-symfony
 
+# Ejecutar el contenedor con las variables de entorno correctas
+sudo docker run -d -p 80:80 -e APP_ENV=prod -e APP_DEBUG=0 --name mywall_container ferminromero00/mywall-symfony:latest
