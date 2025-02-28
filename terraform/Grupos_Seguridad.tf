331111e1bdf094fe133b1bf3ca57bd0fc861f303
Fermin <ferminromero2005@gmail.com>
2025-02-28 15:32:24 +0100

    Actualizar reglas de seguridad en Grupos_Seguridad.tf: eliminar acceso a JSON-SERVER y agregar acceso a EFS

--- terraform/Grupos_Seguridad.tf
+++ terraform/Grupos_Seguridad.tf
@@ -20,25 +20,20 @@ resource "aws_security_group" "grupo_seguridad_servidor" {
     description = "Acceso SSH desde cualquier origen"
   }
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso JSON-SERVER"
-  }
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Acceso HTTPS desde cualquier origen"
   }
+
+  # Regla de entrada para NFS (EFS)
   ingress {
-    from_port   = 389
-    to_port     = 389
+    from_port   = 2049
+    to_port     = 2049
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso LDAP desde cualquier origen"
+    description = "Acceso a EFS desde cualquier origen"
   }
   # Regla de salida para todo el trafico
   egress {
