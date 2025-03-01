fb30294eab6c1bb16768663094637f91fcbd17a0
Fermin <ferminromero2005@gmail.com>
2025-03-01 13:44:19 +0100

    Agregar instalación de kubectl y eksctl en el script user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -10,8 +10,18 @@ sudo dnf install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Instalar kubectl
+curl -LO "https://dl.k8s.io/release/v1.31.1/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# Instalar eksctl
+curl -Lo eksctl_Linux_amd64.tar.gz https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz
+tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp && rm eksctl_Linux_amd64.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
 # Descargar la imagen de Mywall de DockerHub
-sudo docker pull ferminromero00/mywall-symfony
+# sudo docker pull ferminromero00/mywall-symfony
 
 # Ejecutar el contenedor con las variables de entorno correctas
-sudo docker run -d -p 80:80 -e APP_ENV=prod -e APP_DEBUG=0 --name mywall_container ferminromero00/mywall-symfony:latest
+# sudo docker run -d -p 80:80 -e APP_ENV=prod -e APP_DEBUG=0 --name mywall_container ferminromero00/mywall-symfony:latest
